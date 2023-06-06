fe4d7ce1d4c6748d8fb4649da0e2682cdd82cea6
CORALIE COTTET <cottet_cor>
2023-06-06 17:12:59 +0200

    derniere modif

---
+++ Exemple_quarto/R/Untitled-1.r
@@ -0,0 +1,10 @@
+## ---- coder: Cosaph ---- ##
+## ---- coding utf-8 ---- ##
+
+## ---- library ---- ##
+
+library("rio") #nécéssité d'installer des format ("install.format()")
+
+install_formats()
+
+
---
+++ Exemple_quarto/R/test_r.r
@@ -0,0 +1,9 @@
+## ---- coder: Cosaph ---- ##
+## ---- coding utf-8 ---- ##
+
+## ---- library ---- ##
+
+library("rio") #nécéssité d'installer des format ("install.format()")
+install_formats()
+data_sas<-import("election_leg.csv")
+
